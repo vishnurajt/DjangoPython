49fc3a8726e0f507df327fc7d63c0361c2d3415b
vishnurajt <vishnuraj.therayikkal@gmail.com>
2021-02-03 11:31:32 +0530

    checking flowcontrols

--- flowcontrols/breakconinue.py
+++ flowcontrols/breakconinue.py
@@ -1,8 +1,8 @@
-# for i in range(50):
-#     if (i==25):
-#         continue
-#     print(i)
-# print("control is here")
+for i in range(50):
+    if (i==25):
+        continue
+    print(i)
+print("control is here")
 
 for i in range(50):
     if (i==25):
