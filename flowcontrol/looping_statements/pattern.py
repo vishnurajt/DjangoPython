73a96d82b9c0de477ccf83ea25b5fb19d3b09fd0
vishnurajt <vishnuraj.therayikkal@gmail.com>
2021-02-03 22:18:41 +0530

    pattern commit

---
+++ flowcontrol/looping_statements/pattern.py
@@ -0,0 +1,16 @@
+# for i in range(1,5):
+#     pattern="*"*i
+#     print(pattern)
+
+# pattern=""
+# limit=int(input("enter limit"))
+# for i in range(1,limit+1):
+#     pattern+=str(i)
+#     print(pattern)
+
+pattern=""
+limit=int(input("enter limit"))
+for i in range(1,limit+1):
+        pattern=(str(i)*i)
+        print(pattern)
+
