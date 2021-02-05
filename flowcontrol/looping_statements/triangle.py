11963b3aaa28b740502b93ce1778b2b0ed638407
vishnurajt <vishnuraj.therayikkal@gmail.com>
2021-02-05 09:17:53 +0530

    triangle commit

--- flowcontrol/looping_statements/triangle.py
+++ flowcontrol/looping_statements/triangle.py
@@ -3,9 +3,7 @@ for r in range(1,6):
     for c in range(1,10):
         if (r==5)|(r+c==6)|(c-r==4):
             print("*",end="")
-
         else:
              print(" ",end="")
-        # print()
     print()
 
